87b144da417c4b02ca68cc946fe89639ea2e2a65
zhuuuuchok <haha57207@gmail.com>
2023-09-10 14:11:01 +0300

    make first task

---
+++ task_3.py
@@ -0,0 +1,6 @@
+# generate a range of numbers specified by condition
+for i in range(21):
+    # use if loop to check the parity of a number using integer division by 2
+    if i % 2 == 0:
+        # printing results
+        print(i)  
