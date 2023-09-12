8d6143fc5bb7c04ac74ed8b7808edeffc2073f3a
zhuuuuchok <haha57207@gmail.com>
2023-09-12 21:41:44 +0300

    correcting code

--- task 3/task_3_3.py
+++ task 3/task_3_3.py
@@ -1,19 +1,16 @@
 def main():
     n=int(input("Please, enter number of students: "))
-    # Creating a list where all student data will be recorded
-    students = []
-
+    total_sum_of_marks = 0
     for i in range(n):
         name = input(f"Please,enter name of a student{i+1}: ")
-        mark = int(input(f"Please,enter mark of a student {i+1}: "))
-        # Write data to the tuple in the specified order
-        students.append((name,mark))
+        marks =int(input(f"Please,enter mark of a student {i+1}: "))
+        total_sum_of_marks += marks
+    #print(f'Name of student: {name},mark is {marks}')
+    print(f'{total_sum_of_marks}')
 
-    total_sum_of_marks = 0
-
-    for student in students:
-        # Mark is stored in the 2nd element of the tuple(index=[1])
-        total_sum_of_marks += student[1]
+   # for student in students:
+   #     # Mark is stored in the 2nd element of the tuple(index=[1])
+   #     total_sum_of_marks += student[1]
     
     average = int(total_sum_of_marks / n)
     print(f"Average mark of group is: {average}")
